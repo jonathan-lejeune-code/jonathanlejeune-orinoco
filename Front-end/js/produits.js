2b85d9bc0db206e00ee238ec95bdcfe86e40bf9d
jonathan-lejeune-code <lejeune.jonathan29@outlook.fr>
2020-12-20 15:01:20 +0100

    debut page produit

--- Front-end/js/produits.js
+++ Front-end/js/produits.js
@@ -3,8 +3,33 @@
 const URL ="http://localhost:3000/api/teddies"
 
 
-let container = document.getElementById("product");
+let container = document.getElementById("window-product");
 
 function getid(){                           
 
 }
+
+
+
+/*structure HTML */
+
+    let divProduct = document.createElement('div');
+    window-product.appendChild(divProduct);
+    divProduits.className = 'divProduct';
+    
+
+    let divImg = document.createElement('div');
+    window-product.appendChild(divImg);
+    divImg.className = 'divImg';
+
+    let imgProduct = document.createElement('img');
+    divImg.appendChild(imgProduct);
+    imgProduct.className = 'imgProduct';
+
+    let h2Product = document.createElement('h2');
+    divProduits.appendChild(h2Product);
+    h2Product.textContent = Peluche.name;
+    h2Product.className = 'h2Product';
+    
+    
+    
