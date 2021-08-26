356c20a0f0c8461cc2c4374eb45d731b34e38339
jonathan-lejeune-code <lejeune.jonathan29@outlook.fr>
2021-08-26 15:07:52 +0200

    Mise en ligne

--- Front-end/js/formulaire.js
+++ Front-end/js/formulaire.js
@@ -2,15 +2,14 @@
 const form = document.getElementById('form');
 const formValide = [0, 0, 0, 0, 0];
 const regPaterns = {
-  "Nom":      /^[a-zA-z ]{2,}$/,
-  "Prénom":   /^[a-zA-z ]{2,}$/,
-  "Adresse":  /^[0-9]{1,3}([a-zA-Z ]+)$/,
-  "Ville":    /^[a-zA-z ]{2,}$/,
-  "E-mail":   /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}/
+  "Nom": /^[a-zA-z ]{2,}$/,
+  "Prénom": /^[a-zA-z ]{2,}$/,
+  "Adresse": /^[0-9]{1,3}([a-zA-Z ]+)$/,
+  "Ville": /^[a-zA-z ]{2,}$/,
+  "E-mail": /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}/
 };
 
-function inputTest(value, patern)
-{
+function inputTest(value, patern) {
 
   return patern.test(value);
 }
@@ -24,39 +23,33 @@ let valueOfChild = Object.values(childOfForm);
 console.log(childOfForm)
 console.log(valueOfChild)
 
-function changeCallBack(e){
+function changeCallBack(e) {
 
   let elementPlaceholder = e.target.placeholder;
   let currentId = valueOfChild.indexOf(e.target);
-  let messageElement = document.getElementById("response-"+ e.target.id);
-    
+  let messageElement = document.getElementById("response-" + e.target.id);
 
-  if(!inputTest(e.target.value, regPaterns[elementPlaceholder]))
-  {
+
+  if (!inputTest(e.target.value, regPaterns[elementPlaceholder])) {
     // small
-    messageElement.textContent = "le champs "+elementPlaceholder.toLowerCase()+" n'est pas valide !";
+    messageElement.textContent = "le champs " + elementPlaceholder.toLowerCase() + " n'est pas valide !";
     messageElement.classList.add("visible");
     messageElement.classList.remove("hidden");
 
     formValide[currentId] = 0;
-  }
-  else 
-  {
+  } else {
     formValide[currentId] = 1;
     messageElement.classList.remove("visible");
     messageElement.classList.add("hidden");
-    
+
   }
 
-  if(formValide < 0)
-  {
+  if (formValide < 0) {
     formValide = 0;
-    
-  }
-  else if(formValide > 5)
-  {
+
+  } else if (formValide > 5) {
     formValide = 5;
-    
+
   }
 
   console.log(formValide);
@@ -69,7 +62,7 @@ lastName.addEventListener('change', changeCallBack);
 firstName.addEventListener('change', changeCallBack);
 
 
-address.addEventListener('change',changeCallBack);
+address.addEventListener('change', changeCallBack);
 
 
 city.addEventListener('change', changeCallBack);
@@ -78,19 +71,19 @@ city.addEventListener('change', changeCallBack);
 email.addEventListener('change', changeCallBack);
 
 
-form.addEventListener('submit',(e) =>{
+form.addEventListener('submit', (e) => {
 
   e.preventDefault()
 
-  let  contact =  {
-    firstName : firstName.value,
-    lastName : lastName.value,
-    address : address.value,
-    city : city.value,
-    email : email.value,
+  let contact = {
+    firstName: firstName.value,
+    lastName: lastName.value,
+    address: address.value,
+    city: city.value,
+    email: email.value,
   }
 
-  if (formValide.indexOf(0) == -1){
+  if (formValide.indexOf(0) == -1) {
     //console.log("soumis")
     // console.log(contact)    
     products = productsId;
@@ -99,39 +92,35 @@ form.addEventListener('submit',(e) =>{
       products,
     }
     // console.log(valide)
-    
+
     //j'envoi les donnée avec post
 
-    fetch('http://localhost:3000/api/teddies/order',{
-      method: 'POST',
-      headers : {
-        'content-type': 'application/json'
-      },
-      body : JSON.stringify(valide)
-    })  
-    
-    
-    .then(response => response.json())   
-    .then(response => {
-      localStorage.clear();
-      
-      let envoiCommande = {  
-        orderId : response.orderId ,
-        totalPrixPanier : totalProduitPanier,
-        nameConfirmation : firstName.value,    
-      }    
-      let commande = JSON.stringify(envoiCommande);
-      localStorage.setItem("commande", commande); 
-      window.location = "confirmation.html"
-    })
-  }
+    fetch('http://localhost:3000/https://lj-orinoco.herokuapp.com/api/teddies/order', {
+        method: 'POST',
+        headers: {
+          'content-type': 'application/json'
+        },
+        body: JSON.stringify(valide)
+      })
 
-  else
-  {
+
+      .then(response => response.json())
+      .then(response => {
+        localStorage.clear();
+
+        let envoiCommande = {
+          orderId: response.orderId,
+          totalPrixPanier: totalProduitPanier,
+          nameConfirmation: firstName.value,
+        }
+        let commande = JSON.stringify(envoiCommande);
+        localStorage.setItem("commande", commande);
+        window.location = "confirmation.html"
+      })
+  } else {
     return false
   }
-}) 
+})
 
 
 console.log(form)
-
